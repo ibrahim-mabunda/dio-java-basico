c2dc3889570ffc0996cc6a40040cde16644be7f8
ibrahim-mabunda <murilo.mabunda@gmail.com>
2025-02-25 12:07:07 +0200

    minhaClasse

---
+++ meu-primeiro-java/src/MinhaClasse.java
@@ -0,0 +1,15 @@
+public class MinhaClasse {
+    
+public static void main (string[] args) {
+    
+    string primeiroNome = "João";
+    string segundoNome = "Silva";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+    
+    System.out.println(nomeCompleto);
+}
+
+public static string nomeCompleto (string primeiroNome, string SegundoNome) {}
+    return resultado = primeiroNome + " " + segundoNome;
+}
